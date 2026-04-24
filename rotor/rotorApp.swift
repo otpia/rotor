594e484d707d67d0a434801b191fd99f527f7499
liasica <magicrolan@qq.com>
2026-04-24 13:16:42 +0800

    Initial Commit

---
+++ rotor/rotorApp.swift
@@ -0,0 +1,17 @@
+//
+//  rotorApp.swift
+//  rotor
+//
+//  Created by liasica on 2026/4/24.
+//
+
+import SwiftUI
+
+@main
+struct rotorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
